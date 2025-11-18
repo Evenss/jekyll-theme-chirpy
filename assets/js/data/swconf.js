da895abd0f73fa9dc64672306c6a2efb1da7b3f3
Evenss <Evenss@users.noreply.github.com>
2025-11-18 03:20:52 +0000

    deploy: c2bc5f6d31fe1fbd706a8ecaf353c1fef3bac062

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763435853',resources: [
+    cacheName: 'chirpy-1763436052',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
